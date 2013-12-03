ade0f92eb77820e7b992aeceb8bf8b7f24de84f1
govedarski-tum <govedarski@mytum.de>
2013-12-03 19:15:17 +0100

    pssif.transform module

---
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/TestTransform.java
@@ -0,0 +1,14 @@
+package de.tum.pssif.transform;
+
+import de.tum.pssif.core.metamodel.Metamodel;
+import de.tum.pssif.core.metamodel.impl.MetamodelImpl;
+
+
+public class TestTransform {
+
+  public static void test() {
+    //TODO
+    Metamodel mm = new MetamodelImpl();
+  }
+
+}
