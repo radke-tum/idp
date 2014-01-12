6b98e8e32a8700065d358be0dc814e9f4fc9617a
govedarski-tum <govedarski@mytum.de>
2014-01-12 14:32:18 +0100

    * Mapper API  * Mapper subs for GraphML, Visio and SysML.

--- pssif/pssif.transform/src/main/java/de/tum/pssif/transform/mapper/MapperFactory.java
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/mapper/MapperFactory.java
@@ -1,6 +1,30 @@
 package de.tum.pssif.transform.mapper;
 
+import de.tum.pssif.core.exception.PSSIFException;
+import de.tum.pssif.core.util.PSSIFUtil;
+import de.tum.pssif.transform.mapper.graphml.GraphMlMapper;
+import de.tum.pssif.transform.mapper.sysml.SysMlMapper;
+import de.tum.pssif.transform.mapper.visio.VisioMapper;
+
+
 public final class MapperFactory {
+
+  public static final String GRAPHML = "graphml";
+  public static final String SYSML   = "sysml";
+  public static final String VISIO   = "visio";
+
+  public static Mapper getMapper(String name) {
+    if (PSSIFUtil.areSame(GRAPHML, name)) {
+      return new GraphMlMapper();
+    }
+    else if (PSSIFUtil.areSame(SYSML, name)) {
+      return new SysMlMapper();
+    }
+    else if (PSSIFUtil.areSame(VISIO, name)) {
+      return new VisioMapper();
+    }
+    throw new PSSIFException("No mapper found for name: " + name);
+  }
 
   private MapperFactory() {
     //Nothing here
---
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/mapper/sysml/SysMlMapper.java
@@ -0,0 +1,25 @@
+package de.tum.pssif.transform.mapper.sysml;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import de.tum.pssif.core.metamodel.Metamodel;
+import de.tum.pssif.core.model.Model;
+import de.tum.pssif.transform.mapper.Mapper;
+
+
+public class SysMlMapper implements Mapper {
+
+  @Override
+  public Model read(Metamodel metamodel, InputStream inputStream) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Not supperted yet.");
+  }
+
+  @Override
+  public void write(Metamodel metamodel, Model model, OutputStream outputStream) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Not supperted yet.");
+  }
+
+}
