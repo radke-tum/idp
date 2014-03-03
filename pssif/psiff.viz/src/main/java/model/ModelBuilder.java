cdef91447a8a73db4b6614617431436514c38ed8
Bempel <weilerlu@cs.tum.edu>
2014-03-03 14:55:41 +0100

    Improved FileImport

--- pssif/psiff.viz/src/main/java/model/ModelBuilder.java
+++ pssif/psiff.viz/src/main/java/model/ModelBuilder.java
@@ -46,16 +46,8 @@ import de.tum.pssif.core.metamodel.impl.SetValueOperation;
  */
 public class ModelBuilder {
 	
-	/*public static Model model;
-	public static MutableMetamodel meta;
-	private static MyNodeTypes nodeTypes;
-	private static MyEdgeTypes edgeTypes;
-	private static LinkedList<MyNode> nodes;
-	private static LinkedList<MyEdge> edges;
-	*/
 	private static LinkedList<MyModelContainer> activeModels;
-	/*private static HashMap<MyNode, MyModelContainer> mappingNodeModel;
-	private static HashMap<MyEdge, MyModelContainer> mappingEdgeModel;*/
+
 	
 	/**
 	 * Initializes all the content
@@ -67,13 +59,10 @@ public class ModelBuilder {
 		if (activeModels==null)
 		{
 			activeModels = new LinkedList<MyModelContainer>();
-			/*mappingEdgeModel = new HashMap<MyEdge, MyModelContainer>();
-			mappingNodeModel = new HashMap<MyNode, MyModelContainer>();*/
 		}
 		
 		MyModelContainer newModel = new MyModelContainer(Pmodel, Pmeta);
 		activeModels.add(newModel);	
-		
 		
 	}
 	
@@ -83,8 +72,6 @@ public class ModelBuilder {
 	public ModelBuilder()
 	{
 		activeModels = new LinkedList<MyModelContainer>();
-		/*mappingEdgeModel = new HashMap<MyEdge, MyModelContainer>();
-		mappingNodeModel = new HashMap<MyNode, MyModelContainer>();*/
 	}
 
 	
@@ -133,81 +120,6 @@ public class ModelBuilder {
 		return null;
 	}
 	
-/*	private void mockData ()
-	{
-		meta = new MetamodelImpl();
-	    NodeType development = meta.createNodeType("development artifact");
-
-	    NodeType solution = meta.createNodeType("solution artifact");
-	    solution.inherit(development);
-	    NodeType hardware = meta.createNodeType("hardware");
-	    hardware.inherit(solution);
-	    NodeType software = meta.createNodeType("software");
-	    software.inherit(solution);
-	    NodeType usecase = meta.createNodeType("usecase");
-	    usecase.inherit(development);
-	    NodeType requirement = meta.createNodeType("requirement");
-	    requirement.inherit(development);
-
-	    EdgeType containment = meta.createEdgeType("containment");
-	    containment.createMapping("contains", hardware, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 0, UnlimitedNatural.UNLIMITED),
-	        "contained in", hardware, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 1, UnlimitedNatural.of(1)));
-	    containment.createMapping("contains", hardware, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 0, UnlimitedNatural.UNLIMITED),
-	        "contained in", software, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 1, UnlimitedNatural.of(1)));
-	    containment.createMapping("contains", software, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 0, UnlimitedNatural.UNLIMITED),
-	        "contained in", software, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 1, UnlimitedNatural.of(1)));
-	    
-	    EdgeType uses = meta.createEdgeType("uses");
-	    uses.createMapping("uses", hardware, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 0, UnlimitedNatural.UNLIMITED),
-		        "used by", software, MultiplicityContainer.of(1, UnlimitedNatural.of(1), 1, UnlimitedNatural.of(1)));
-	    
-	    node("hardware", meta).createAttribute(node("hardware", meta).getDefaultAttributeGroup(), "testattr", PrimitiveDataType.INTEGER, Units.NONE, true, AttributeCategory.METADATA);
-	    node("hardware", meta).createAttribute(node("hardware", meta).getDefaultAttributeGroup(), "testattr2", PrimitiveDataType.BOOLEAN, Units.NONE, true, AttributeCategory.METADATA);
-	    node("hardware", meta).createAttribute(node("hardware", meta).getDefaultAttributeGroup(), "testattr3", PrimitiveDataType.DECIMAL, Units.KILOGRAM, true, AttributeCategory.METADATA);
-	    
-	    
-	    model = new ModelImpl();
-	    
-	    Node ebike = node("hardware", meta).create(model);
-	    node("hardware", meta).findAttribute("testattr").set(ebike, PSSIFOption.one(PSSIFValue.create(5)));
-	    node("hardware", meta).findAttribute("testattr2").set(ebike, PSSIFOption.one(PSSIFValue.create(true)));
-	    node("hardware", meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(ebike, PSSIFOption.one(PSSIFValue.create("Ebike")));
-	    Node smartphone = node("hardware", meta).create(model);
-	    node("hardware", meta).findAttribute("testattr2").set(smartphone, PSSIFOption.one(PSSIFValue.create(true)));
-	    node("hardware", meta).findAttribute("testattr").set(smartphone, PSSIFOption.one(PSSIFValue.create(10)));
-	    node("hardware", meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(smartphone, PSSIFOption.one(PSSIFValue.create("Smartphone")));
-	    Node battery = node("hardware", meta).create(model);
-	    node("hardware", meta).findAttribute("testattr2").set(battery, PSSIFOption.one(PSSIFValue.create(false)));
-	    node("hardware", meta).findAttribute("testattr").set(battery, PSSIFOption.one(PSSIFValue.create(15)));
-	    node("hardware", meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(battery, PSSIFOption.one(PSSIFValue.create("Battery")));
-	    Node rentalApp = node("software", meta).create(model);
-	    node("software", meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(rentalApp, PSSIFOption.one(PSSIFValue.create("RentalApp")));
-	    Node gpsApp = node("software", meta).create(model);
-	    node("software", meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(gpsApp, PSSIFOption.one(PSSIFValue.create("GpsApp")));
-	   // node("hardware", meta).createAttribute(group, name, dataType, visible, category)
-	    
-	    EdgeType hwContainment = node("hardware", meta).findOutgoingEdgeType("containment");
-	    ConnectionMapping hw2hw = hwContainment.getMapping(node("hardware", meta), node("hardware", meta));
-	    ConnectionMapping hw2sw = hwContainment.getMapping(node("hardware", meta), node("software", meta));
-	    
-
-	    
-	    
-	    
-	    hw2hw.create(model, ebike, battery);
-	    hw2hw.create(model, ebike, smartphone);
-	    hw2sw.create(model, smartphone, rentalApp);
-	    
-	    EdgeType hwUses = node("hardware", meta).findOutgoingEdgeType("uses");
-	    ConnectionMapping hw2swUses = hwUses.getMapping(node("hardware", meta), node("software", meta));
-	    
-	    hw2swUses.create(model, smartphone, gpsApp);
-	  }
-	
-   private static NodeType node(String name, MutableMetamodel metamodel) {
-	   return metamodel.findNodeType(name);
-   }*/
-
 	public static MyNodeTypes getNodeTypes() {
 		HashSet<MyNodeType> tmp = new HashSet<MyNodeType>();
 		
@@ -234,7 +146,6 @@ public class ModelBuilder {
 	
 	public static void addCollapserEdge(MyEdge newEdge)
 	{
-		//MyEdge2 newEdge = new MyEdge2(edge, type, source, destination);
 		//TODO no very good implementation
 		newEdge.setCollapseEdge(true);
 		activeModels.getFirst().addEdge(newEdge);
@@ -266,42 +177,16 @@ public class ModelBuilder {
 	
 	public static void addNewNodeFromGUI (String nodeName, MyNodeType type)
 	{
-		/*Node newNode = node(type.getName(), meta).create(model);
-		
-		node(type.getName(), meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(newNode, PSSIFOption.one(PSSIFValue.create(nodeName)));
-		
-		nodes.add(new MyNode(newNode, type));*/
 		activeModels.getFirst().addNewNodeFromGUI(nodeName, type);
 	}
 	
 	public static void removeNode (MyNode node)
 	{
-		//node.getNode().apply(new Disco);
-		
-		//Node newNode = node(type.getName(), meta)
-		
-		//node(type.getName(), meta).findAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).set(newNode, PSSIFOption.one(PSSIFValue.create(nodeName)));
-		
-		//newNode.apply();
-		//node.getNode().apply(new DisconnectOperation(null, null, null));
+		throw new NullPointerException("Is not implemented in the core");
 	}
 	
 	public static boolean addNewEdgeGUI(MyNode source, MyNode destination, MyEdgeType edgetype)
 	{
-		/*ConnectionMapping mapping = edgetype.getType().getMapping(source.getNodeType().getType(), destination.getNodeType().getType());
-		
-		if (mapping!=null)
-		{
-			Edge newEdge = mapping.create(model, source.getNode(), destination.getNode());
-				
-			MyEdge e  = new MyEdge(newEdge, edgetype, source, destination);
-			
-			edges.add(e);
-			return true;
-		}
-		else
-			return false;*/
 		return activeModels.getFirst().addNewEdgeGUI(source, destination, edgetype);
-		
 	}
 }
