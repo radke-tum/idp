d363e43747f3df7b1f6b53d258bba53a69468078
kgo <kgo@192.168.37.114>
2014-03-06 20:37:07 +0100

    SysML: Stuff

--- pssif/pssif.transform/src/main/java/de/tum/pssif/transform/io/SysMlIoMapper.java
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/io/SysMlIoMapper.java
@@ -3,6 +3,11 @@ package de.tum.pssif.transform.io;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+
 import de.tum.pssif.transform.IoMapper;
 import de.tum.pssif.transform.graph.Graph;
 
@@ -11,8 +16,39 @@ public class SysMlIoMapper implements IoMapper {
 
   @Override
   public Graph read(InputStream in) {
-    // TODO Auto-generated method stub
-    return null;
+
+    XMLInputFactory factory = XMLInputFactory.newInstance();
+
+    try {
+      XMLStreamReader reader = factory.createXMLStreamReader(in);
+      Graph graph = new Graph();
+
+      while (reader.hasNext()) {
+        int event = reader.next();
+
+        switch (event) {
+          case XMLEvent.START_ELEMENT:
+            startElement(reader);
+            break;
+          case XMLEvent.END_ELEMENT:
+            endElement(reader);
+            break;
+        }
+      }
+
+      reader.close();
+      return graph;
+    } catch (XMLStreamException e) {
+      throw new PSSIFIoException("Failed to read SysML XML", e);
+    }
+  }
+
+  private void startElement(XMLStreamReader reader) throws XMLStreamException {
+    //TODO
+  }
+
+  private void endElement(XMLStreamReader reader) throws XMLStreamException {
+    //TODO
   }
 
   @Override
