839e55348b3549c50d510a022b852957bdcea723
kgo <kgo@kgo-PC.iteratec.de>
2014-03-09 09:52:41 +0100

    SysML: more local

--- pssif/pssif.transform/src/main/java/de/tum/pssif/transform/io/SysMlIoMapper.java
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/io/SysMlIoMapper.java
@@ -2,6 +2,7 @@ package de.tum.pssif.transform.io;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.UUID;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -10,32 +11,26 @@ import javax.xml.stream.events.XMLEvent;
 
 import de.tum.pssif.transform.IoMapper;
 import de.tum.pssif.transform.graph.Graph;
+import de.tum.pssif.transform.io.SysMlIoMapperUml.UmlModel;
+import de.tum.pssif.transform.io.SysMlIoMapperUml.UmlPackagedElement;
 
 
 public class SysMlIoMapper implements IoMapper {
 
   @Override
   public Graph read(InputStream in) {
-
     XMLInputFactory factory = XMLInputFactory.newInstance();
-
     try {
       XMLStreamReader reader = factory.createXMLStreamReader(in, "UTF-8");
       Graph graph = new Graph();
-
       while (reader.hasNext()) {
         int event = reader.next();
-
         switch (event) {
           case XMLEvent.START_ELEMENT:
             startElement(reader);
             break;
-          case XMLEvent.END_ELEMENT:
-            endElement(reader);
-            break;
         }
       }
-
       reader.close();
       return graph;
     } catch (XMLStreamException e) {
@@ -46,20 +41,28 @@ public class SysMlIoMapper implements IoMapper {
   private void startElement(XMLStreamReader reader) throws XMLStreamException {
     String elementName = reader.getName().getLocalPart();
     String elementNs = reader.getName().getPrefix();
-    //TODO
-
     if (SysMLTokens.UML.equalsIgnoreCase(elementNs) && SysMLTokens.MODEL.equalsIgnoreCase(elementName)) {
-      readUmlModel();
+      readUmlModel(reader);
     }
-
-    //fucking piece of sh...
-    //1. read the uml class diagram with the UML2/4.0.0 eCore
-    //2. read the rest of the file into some DS.
-    //3. make something out of it...
-    System.out.println("Ëlement: " + elementNs + ":" + elementName);
+    else if (SysMLTokens.SYS_ML.equals(elementNs)) {
+      readSysMlProfile(reader);
+    }
+    else if (SysMLTokens.SFB768.equalsIgnoreCase(elementNs)) {
+      readSfb768Element(reader);
+    }
   }
 
-  private void endElement(XMLStreamReader reader) throws XMLStreamException {
+  private void readUmlModel(XMLStreamReader reader) throws XMLStreamException {
+    String modelId = reader.getAttributeValue(SysMLTokens.XML_NS_XMI_URI, SysMLTokens.ID);
+    String modelName = reader.getAttributeValue(null, SysMLTokens.NAME);
+    UmlModel model = new UmlModel(UUID.fromString(modelId), modelName);
+  }
+
+  private void readSysMlProfile(XMLStreamReader eader) {
+    //TODO
+  }
+
+  private void readSfb768Element(XMLStreamReader reader) {
     //TODO
   }
 
@@ -67,6 +70,14 @@ public class SysMlIoMapper implements IoMapper {
   public void write(Graph graph, OutputStream out) {
     // TODO Auto-generated method stub
 
+  }
+
+  private static class ReadContext {
+
+    int                phase = 0;
+
+    UmlModel           umlModel;
+    UmlPackagedElement currentPackagedElement;
   }
 
 }
