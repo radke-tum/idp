26d68a03d579c3943cb173cf91c30979fbbc2559
Bernhard Radke <bernhard.radke@tum.de>
2014-03-09 18:05:02 +0100

    removed obsolete code

--- pssif/pssif.transform/src/main/java/de/tum/pssif/transform/transformation/delegating/DelegatingEdgeType.java
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/transformation/delegating/DelegatingEdgeType.java
@@ -1,6 +1,5 @@
 package de.tum.pssif.transform.transformation.delegating;
 
-import de.tum.pssif.core.common.PSSIFOption;
 import de.tum.pssif.core.metamodel.ConnectionMapping;
 import de.tum.pssif.core.metamodel.NodeTypeBase;
 import de.tum.pssif.core.metamodel.impl.EdgeTypeImpl;
@@ -10,12 +9,6 @@ import de.tum.pssif.transform.transformation.viewed.ViewedConnectionMapping;
 public class DelegatingEdgeType extends EdgeTypeImpl {
   public DelegatingEdgeType(String name) {
     super(name);
-  }
-
-  @Override
-  public PSSIFOption<ConnectionMapping> getMapping(NodeTypeBase from, NodeTypeBase to) {
-    // TODO Auto-generated method stub
-    return super.getMapping(from, to);
   }
 
   /*package*/void delegate(ConnectionMapping baseMapping, NodeTypeBase from, NodeTypeBase to) {
