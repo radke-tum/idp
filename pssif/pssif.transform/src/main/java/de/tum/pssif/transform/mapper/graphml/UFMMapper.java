3112538de5218bbc360d5a2c00ed99279db1eabe
Bernhard Radke <bernhard.radke@tum.de>
2014-03-10 20:46:34 +0100

    beauty on view creation

--- pssif/pssif.transform/src/main/java/de/tum/pssif/transform/mapper/graphml/UFMMapper.java
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/mapper/graphml/UFMMapper.java
@@ -9,6 +9,7 @@ import de.tum.pssif.core.metamodel.ConnectionMapping;
 import de.tum.pssif.core.metamodel.EdgeType;
 import de.tum.pssif.core.metamodel.Metamodel;
 import de.tum.pssif.core.metamodel.NodeType;
+import de.tum.pssif.core.metamodel.PSSIFCanonicMetamodelCreator;
 import de.tum.pssif.core.model.Model;
 import de.tum.pssif.transform.transformation.CreateArtificialEdgeTransformation;
 import de.tum.pssif.transform.transformation.CreateArtificialNodeTransformation;
@@ -24,6 +25,23 @@ import de.tum.pssif.transform.transformation.RenameNodeTypeTransformation;
 
 
 public class UFMMapper extends GraphMLMapper {
+  private static final String E_MATERIAL_FLOW    = "MaterialFlow";
+  private static final String E_ENERGY_FLOW      = "EnergyFlow";
+  private static final String E_CONTROL_FLOW     = PSSIFCanonicMetamodelCreator.E_FLOW_CONTROL;
+  private static final String E_INFORMATION_FLOW = "InformationFlow";
+  private static final String A_COST             = PSSIFCanonicMetamodelCreator.A_BLOCK_COST;
+  private static final String A_DURATION         = PSSIFCanonicMetamodelCreator.A_DURATION;
+  private static final String A_PRIORITY         = PSSIFCanonicMetamodelCreator.A_REQUIREMENT_PRIORITY;
+  private static final String A_TYPE             = PSSIFCanonicMetamodelCreator.A_REQUIREMENT_TYPE;
+  private static final String A_WEIGHT           = PSSIFCanonicMetamodelCreator.A_HARDWARE_WEIGHT;
+  private static final String A_FUNCTIONARY      = "functionary";
+  private static final String N_STATE            = PSSIFCanonicMetamodelCreator.N_STATE;
+  private static final String E_RELATIONSHIP     = PSSIFCanonicMetamodelCreator.E_RELATIONSHIP;
+  private static final String N_BLOCK            = PSSIFCanonicMetamodelCreator.N_BLOCK;
+  private static final String N_FUNCTION         = PSSIFCanonicMetamodelCreator.N_FUNCTION;
+  private static final String N_REQUIREMENT      = PSSIFCanonicMetamodelCreator.N_REQUIREMENT;
+  private static final String N_HARDWARE         = PSSIFCanonicMetamodelCreator.N_HARDWARE;
+
   @Override
   public Model read(Metamodel metamodel, InputStream inputStream) {
     return readInternal(createUfpView(metamodel), inputStream);
@@ -35,80 +53,97 @@ public class UFMMapper extends GraphMLMapper {
   }
 
   public static Metamodel createUfpView(Metamodel metamodel) {
-    Metamodel view = new RenameEdgeTypeTransformation(metamodel.getEdgeType("Information Flow").getOne(), "InformationFlow").apply(metamodel);
-    view = new RenameEdgeTypeTransformation(view.getEdgeType("Energy Flow").getOne(), "EnergyFlow").apply(view);
-    view = new RenameEdgeTypeTransformation(view.getEdgeType("Material Flow").getOne(), "MaterialFlow").apply(view);
-    view = new RenameNodeTypeTransformation(view.getNodeType("Function").getOne(), "AbstractFunction").apply(view);
-    view = new RenameNodeTypeTransformation(view.getNodeType("Activity").getOne(), "Function").apply(view);
-    NodeType rootNode = view.getNodeType(PSSIFConstants.ROOT_NODE_TYPE_NAME).getOne();
-    EdgeType rootEdge = view.getEdgeType(PSSIFConstants.ROOT_EDGE_TYPE_NAME).getOne();
-    view = new HideNodeTypeAttributeTransformation(rootNode, rootNode.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_COMMENT).getOne()).apply(view);
-    view = new HideNodeTypeAttributeTransformation(rootNode, rootNode.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_END).getOne())
-        .apply(view);
-    view = new HideNodeTypeAttributeTransformation(rootNode, rootNode.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_START).getOne())
-        .apply(view);
-    view = new HideNodeTypeAttributeTransformation(rootNode, rootNode.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VERSION).getOne()).apply(view);
-    view = new HideEdgeTypeAttributeTransformation(rootEdge, rootEdge.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_COMMENT).getOne()).apply(view);
-    view = new HideEdgeTypeAttributeTransformation(rootEdge, rootEdge.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_END).getOne())
-        .apply(view);
-    view = new HideEdgeTypeAttributeTransformation(rootEdge, rootEdge.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_START).getOne())
-        .apply(view);
-    view = new HideEdgeTypeAttributeTransformation(rootEdge, rootEdge.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VERSION).getOne()).apply(view);
+    Metamodel view = new RenameEdgeTypeTransformation(et(PSSIFCanonicMetamodelCreator.E_FLOW_INFORMATION, metamodel), E_INFORMATION_FLOW)
+        .apply(metamodel);
+    view = new RenameEdgeTypeTransformation(et(PSSIFCanonicMetamodelCreator.E_FLOW_ENERGY, view), E_ENERGY_FLOW).apply(view);
+    view = new RenameEdgeTypeTransformation(et(PSSIFCanonicMetamodelCreator.E_FLOW_MATERIAL, view), E_MATERIAL_FLOW).apply(view);
+    view = new RenameNodeTypeTransformation(nt(N_FUNCTION, view), "AbstractFunction").apply(view);
+    view = new RenameNodeTypeTransformation(nt("Activity", view), N_FUNCTION).apply(view);
 
-    view = new HideNodeTypeAttributeTransformation(view.getNodeType("Hardware").getOne(), view.getNodeType("Hardware").getOne()
-        .getAttribute("weight").getOne()).apply(view);
-    view = new HideNodeTypeAttributeTransformation(view.getNodeType("Requirement").getOne(), view.getNodeType("Requirement").getOne()
-        .getAttribute("type").getOne()).apply(view);
-    view = new HideNodeTypeAttributeTransformation(view.getNodeType("Requirement").getOne(), view.getNodeType("Requirement").getOne()
-        .getAttribute("priority").getOne()).apply(view);
-    view = new HideNodeTypeAttributeTransformation(view.getNodeType("Function").getOne(), view.getNodeType("Function").getOne()
-        .getAttribute("duration").getOne()).apply(view);
-    view = new HideNodeTypeAttributeTransformation(view.getNodeType("Block").getOne(), view.getNodeType("Block").getOne().getAttribute("cost")
-        .getOne()).apply(view);
+    //hide attributes
+    view = new HideNodeTypeAttributeTransformation(node(view), node(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_COMMENT).getOne())
+        .apply(view);
+    view = new HideNodeTypeAttributeTransformation(node(view), node(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_END).getOne())
+        .apply(view);
+    view = new HideNodeTypeAttributeTransformation(node(view), node(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_START).getOne())
+        .apply(view);
+    view = new HideNodeTypeAttributeTransformation(node(view), node(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VERSION).getOne())
+        .apply(view);
+    view = new HideEdgeTypeAttributeTransformation(edge(view), edge(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_COMMENT).getOne())
+        .apply(view);
+    view = new HideEdgeTypeAttributeTransformation(edge(view), edge(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_END).getOne())
+        .apply(view);
+    view = new HideEdgeTypeAttributeTransformation(edge(view), edge(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VALIDITY_START).getOne())
+        .apply(view);
+    view = new HideEdgeTypeAttributeTransformation(edge(view), edge(view).getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_VERSION).getOne())
+        .apply(view);
+    view = new HideNodeTypeAttributeTransformation(nt(N_HARDWARE, view), nt(N_HARDWARE, view).getAttribute(A_WEIGHT).getOne()).apply(view);
+    view = new HideNodeTypeAttributeTransformation(nt(N_REQUIREMENT, view), nt(N_REQUIREMENT, view).getAttribute(A_TYPE).getOne()).apply(view);
+    view = new HideNodeTypeAttributeTransformation(nt(N_REQUIREMENT, view), nt(N_REQUIREMENT, view).getAttribute(A_PRIORITY).getOne()).apply(view);
+    view = new HideNodeTypeAttributeTransformation(nt(N_FUNCTION, view), nt(N_FUNCTION, view).getAttribute(A_DURATION).getOne()).apply(view);
+    view = new HideNodeTypeAttributeTransformation(nt(N_BLOCK, view), nt(N_BLOCK, view).getAttribute(A_COST).getOne()).apply(view);
 
-    EdgeType relationship = view.getEdgeType("Relationship").getOne();
-    ConnectionMapping s2b = relationship.getMapping(view.getNodeType("State").getOne(), view.getNodeType("Block").getOne()).getOne();
-    ConnectionMapping f2b = relationship.getMapping(view.getNodeType("Function").getOne(), view.getNodeType("Block").getOne()).getOne();
+    //get relationship mappings for future use
+    EdgeType relationship = view.getEdgeType(E_RELATIONSHIP).getOne();
+    ConnectionMapping s2b = relationship.getMapping(nt(N_STATE, view), nt(N_BLOCK, view)).getOne();
+    ConnectionMapping f2b = relationship.getMapping(nt(N_FUNCTION, view), nt(N_BLOCK, view)).getOne();
 
-    NodeType block = view.getNodeType("Block").getOne();
+    //create artificial blocks for function and state
+    view = new CreateArtificialNodeTransformation(nt(N_FUNCTION, view), et(E_RELATIONSHIP, view), nt(N_BLOCK, view)).apply(view);
+    view = new CreateArtificialNodeTransformation(nt(N_STATE, view), et(E_RELATIONSHIP, view), nt(N_BLOCK, view)).apply(view);
+
+    //move functionary attribute as id and name attribute via relationship to connected blocks
+    NodeType block = nt(N_BLOCK, view);
     Attribute id = block.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_ID).getOne();
-    view = new MoveAttributeTransformation(view.getNodeType("Function").getOne(), "functionary", block, id, view.getEdgeType("Relationship").getOne())
-        .apply(view);
+    view = new MoveAttributeTransformation(nt(N_FUNCTION, view), A_FUNCTIONARY, block, id, et(E_RELATIONSHIP, view)).apply(view);
     Attribute name = block.getAttribute(PSSIFConstants.BUILTIN_ATTRIBUTE_NAME).getOne();
-    view = new MoveAttributeTransformation(view.getNodeType("Function").getOne(), "functionary", block, name, view.getEdgeType("Relationship")
-        .getOne()).apply(view);
+    view = new MoveAttributeTransformation(nt(N_FUNCTION, view), A_FUNCTIONARY, block, name, et(E_RELATIONSHIP, view)).apply(view);
+    view = new MoveAttributeTransformation(nt(N_STATE, view), A_FUNCTIONARY, block, id, et(E_RELATIONSHIP, view)).apply(view);
+    view = new MoveAttributeTransformation(nt(N_STATE, view), A_FUNCTIONARY, block, name, et(E_RELATIONSHIP, view)).apply(view);
 
-    view = new MoveAttributeTransformation(view.getNodeType("State").getOne(), "functionary", block, id, view.getEdgeType("Relationship").getOne())
+    //join the informationflow to connect the artificial blocks
+    view = joinToArtificialBlocks(E_INFORMATION_FLOW, s2b, f2b, view);
+    view = joinToArtificialBlocks(E_ENERGY_FLOW, s2b, f2b, view);
+    view = joinToArtificialBlocks(E_MATERIAL_FLOW, s2b, f2b, view);
+
+    //create the artificial control flows
+    view = new CreateArtificialEdgeTransformation(nt(N_STATE, view), nt(N_FUNCTION, view), et(E_INFORMATION_FLOW, view), et(E_CONTROL_FLOW, view))
         .apply(view);
-    view = new MoveAttributeTransformation(view.getNodeType("State").getOne(), "functionary", block, name, view.getEdgeType("Relationship").getOne())
+    view = new CreateArtificialEdgeTransformation(nt(N_FUNCTION, view), nt(N_STATE, view), et(E_INFORMATION_FLOW, view), et(E_CONTROL_FLOW, view))
         .apply(view);
 
-    view = new CreateArtificialNodeTransformation(view.getNodeType("Function").getOne(), view.getEdgeType("Relationship").getOne(), view.getNodeType(
-        "Block").getOne()).apply(view);
-    view = new CreateArtificialNodeTransformation(view.getNodeType("State").getOne(), view.getEdgeType("Relationship").getOne(), view.getNodeType(
-        "Block").getOne()).apply(view);
+    //avoid instances of control flow within the viewed model
+    //TODO deinstantify
+    view = new HideEdgeTypeTransformation(et(E_CONTROL_FLOW, view)).apply(view);
 
-    view = new JoinLeftOutgoingTransformation(view.getEdgeType("InformationFlow").getOne(), s2b, view.getNodeType("State").getOne(), view
-        .getNodeType("Function").getOne()).apply(view);
-    view = new JoinLeftOutgoingTransformation(view.getEdgeType("InformationFlow").getOne(), f2b, view.getNodeType("Function").getOne(), view
-        .getNodeType("State").getOne()).apply(view);
-
-    view = new JoinRightOutgoingTransformation(view.getEdgeType("InformationFlow").getOne(), s2b, view.getNodeType("Function").getOne(), view
-        .getNodeType("State").getOne()).apply(view);
-    view = new JoinRightOutgoingTransformation(view.getEdgeType("InformationFlow").getOne(), f2b, view.getNodeType("State").getOne(), view
-        .getNodeType("Function").getOne()).apply(view);
-
-    view = new CreateArtificialEdgeTransformation(view.getNodeType("State").getOne(), view.getNodeType("Function").getOne(), view.getEdgeType(
-        "InformationFlow").getOne(), view.getEdgeType("Control Flow").getOne()).apply(view);
-    view = new CreateArtificialEdgeTransformation(view.getNodeType("Function").getOne(), view.getNodeType("State").getOne(), view.getEdgeType(
-        "InformationFlow").getOne(), view.getEdgeType("Control Flow").getOne()).apply(view);
-
-    view = new HideEdgeTypeTransformation(view.getEdgeType("Relationship").getOne()).apply(view);
-
-    view = new HideEdgeTypeTransformation(view.getEdgeType("Control Flow").getOne()).apply(view);
-
+    //avoid instances of block within the viewed model
+    //TODO deinstantify
     view = new HideNodeTypeTransformation(block).apply(view);
 
     return view;
+  }
+
+  protected static Metamodel joinToArtificialBlocks(String etName, ConnectionMapping s2b, ConnectionMapping f2b, Metamodel view) {
+    view = new JoinLeftOutgoingTransformation(et(etName, view), s2b, nt(N_STATE, view), nt(N_FUNCTION, view)).apply(view);
+    view = new JoinLeftOutgoingTransformation(et(etName, view), f2b, nt(N_FUNCTION, view), nt(N_STATE, view)).apply(view);
+    view = new JoinRightOutgoingTransformation(et(etName, view), s2b, nt(N_FUNCTION, view), nt(N_STATE, view)).apply(view);
+    view = new JoinRightOutgoingTransformation(et(etName, view), f2b, nt(N_STATE, view), nt(N_FUNCTION, view)).apply(view);
+    return view;
+  }
+
+  private static NodeType nt(String name, Metamodel metamodel) {
+    return metamodel.getNodeType(name).getOne();
+  }
+
+  private static EdgeType et(String name, Metamodel metamodel) {
+    return metamodel.getEdgeType(name).getOne();
+  }
+
+  private static NodeType node(Metamodel metamodel) {
+    return nt(PSSIFConstants.ROOT_NODE_TYPE_NAME, metamodel);
+  }
+
+  private static EdgeType edge(Metamodel metamodel) {
+    return et(PSSIFConstants.ROOT_EDGE_TYPE_NAME, metamodel);
   }
 }
---
+++ pssif/pssif.transform/src/test/java/de/tum/pssif/transform/mapper/roundtrip/UFM2EPK.java
@@ -0,0 +1,25 @@
+package de.tum.pssif.transform.mapper.roundtrip;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import de.tum.pssif.core.metamodel.Metamodel;
+import de.tum.pssif.core.metamodel.PSSIFCanonicMetamodelCreator;
+import de.tum.pssif.transform.Mapper;
+import de.tum.pssif.transform.MapperFactory;
+
+
+public class UFM2EPK {
+  @Test
+  public void testRoundtrip() throws FileNotFoundException {
+    Metamodel metamodel = PSSIFCanonicMetamodelCreator.create();
+
+    InputStream in = getClass().getResourceAsStream("/flow.graphml");
+    Mapper ufmMapper = MapperFactory.getMapper(MapperFactory.UOFP);
+    Mapper epkMapper = MapperFactory.getMapper(MapperFactory.EPK);
+    epkMapper.write(metamodel, ufmMapper.read(metamodel, in), new FileOutputStream("target/UFM2EPK_Roundtrip.vsdx"));
+  }
+}
