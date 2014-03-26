45a93e6c6e2004c17182a0a8e0a2072847d34671
Bernhard Radke <bernhard.radke@tum.de>
2014-03-26 19:53:53 +0100

    EdgeType apply inheritance

--- pssif/pssif.core/src/main/java/de/tum/pssif/core/metamodel/impl/EdgeTypeImpl.java
+++ pssif/pssif.core/src/main/java/de/tum/pssif/core/metamodel/impl/EdgeTypeImpl.java
@@ -20,6 +20,9 @@ import de.tum.pssif.core.metamodel.NodeTypeBase;
 import de.tum.pssif.core.metamodel.mutable.MutableAttributeGroup;
 import de.tum.pssif.core.metamodel.mutable.MutableConnectionMapping;
 import de.tum.pssif.core.metamodel.mutable.MutableEdgeType;
+import de.tum.pssif.core.model.Edge;
+import de.tum.pssif.core.model.Model;
+import de.tum.pssif.core.model.Node;
 
 
 public class EdgeTypeImpl extends ElementTypeImpl implements MutableEdgeType {
@@ -191,5 +194,66 @@ public class EdgeTypeImpl extends ElementTypeImpl implements MutableEdgeType {
         return input.getTo().isAssignableFrom(to);
       }
     }));
+  }
+
+  @Override
+  public Node applyFrom(Edge edge) {
+    return new ReadFromNodesOperation().apply(edge);
+  }
+
+  @Override
+  public Node applyTo(Edge edge) {
+    return new ReadToNodesOperation().apply(edge);
+  }
+
+  @Override
+  public PSSIFOption<Edge> apply(Model model, boolean includeSubtypes) {
+    PSSIFOption<Edge> result = PSSIFOption.none();
+
+    for (ConnectionMapping mapping : getMappings().getMany()) {
+      result = PSSIFOption.merge(result, mapping.apply(model));
+    }
+
+    if (includeSubtypes) {
+      for (EdgeType special : getSpecials()) {
+        result = PSSIFOption.merge(result, special.apply(model, includeSubtypes));
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public PSSIFOption<Edge> applyOutgoing(Node node, boolean includeSubtypes) {
+    PSSIFOption<Edge> result = PSSIFOption.none();
+
+    for (ConnectionMapping mapping : getMappings().getMany()) {
+      result = PSSIFOption.merge(result, mapping.applyOutgoing(node));
+    }
+
+    if (includeSubtypes) {
+      for (EdgeType special : getSpecials()) {
+        result = PSSIFOption.merge(result, special.applyOutgoing(node, includeSubtypes));
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public PSSIFOption<Edge> applyIncoming(Node node, boolean includeSubtypes) {
+    PSSIFOption<Edge> result = PSSIFOption.none();
+
+    for (ConnectionMapping mapping : getMappings().getMany()) {
+      result = PSSIFOption.merge(result, mapping.applyIncoming(node));
+    }
+
+    if (includeSubtypes) {
+      for (EdgeType special : getSpecials()) {
+        result = PSSIFOption.merge(result, special.applyIncoming(node, includeSubtypes));
+      }
+    }
+
+    return result;
   }
 }
