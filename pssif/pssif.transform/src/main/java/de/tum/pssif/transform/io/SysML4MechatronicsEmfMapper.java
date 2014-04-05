f9b6f5525839f63ba20a637b67628612b8c1ea4f
kgo <kgo@192.168.37.116>
2014-04-05 21:20:34 +0200

    SysML: SysML4MechatronicsEmfMapper

---
+++ pssif/pssif.transform/src/main/java/de/tum/pssif/transform/io/SysML4MechatronicsEmfMapper.java
@@ -0,0 +1,26 @@
+package de.tum.pssif.transform.io;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import de.tum.pssif.transform.IoMapper;
+import de.tum.pssif.transform.graph.Graph;
+
+
+public class SysML4MechatronicsEmfMapper implements IoMapper {
+
+  private static final String ECORE_RESOURCE = "";
+
+  @Override
+  public Graph read(InputStream in) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void write(Graph graph, OutputStream out) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
