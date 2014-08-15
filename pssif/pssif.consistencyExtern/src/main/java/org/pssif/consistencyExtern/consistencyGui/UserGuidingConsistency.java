93abd3b1f15ea0986b1f944aa00353f26072ccba
Andreas Genz <andreas_genz@me.com>
2014-08-15 12:40:59 +0200

    updated license in files

--- pssif/pssif.consistencyExtern/src/main/java/org/pssif/consistencyExtern/consistencyGui/UserGuidingConsistency.java
+++ pssif/pssif.consistencyExtern/src/main/java/org/pssif/consistencyExtern/consistencyGui/UserGuidingConsistency.java
@@ -31,23 +31,23 @@ import de.tum.pssif.core.metamodel.Metamodel;
 import de.tum.pssif.core.model.Model;
 
 /**
-This project is part of the Product-Service System integration framework. It is responsible for keeping consistency between different requirements models or versions of models.
+This file is part of PSSIF Consistency. It is responsible for keeping consistency between different requirements models or versions of models.
 Copyright (C) 2014 Andreas Genz
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    PSSIF Consistency is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    PSSIF Consistency is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Feel free to contact me via eMail: genz@in.tum.de
+    You should have received a copy of the GNU General Public License
+    along with PSSIF Consistency.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Feel free to contact me via eMail: genz@in.tum.de
 */
 
 /**
