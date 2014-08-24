a607d9656a99f3a76b9b7cf72ced8426243e06fa
Andreas Genz <andreas_genz@me.com>
2014-08-24 11:34:07 +0200

    updated licenses

--- pssif/pssif.consistency/src/main/java/org/pssif/textNormalization/GermanStemming.java
+++ pssif/pssif.consistency/src/main/java/org/pssif/textNormalization/GermanStemming.java
@@ -29,6 +29,26 @@
 package org.pssif.textNormalization;
 
 /**
+This file is part of PSSIF Consistency. It is responsible for keeping consistency between different requirements models or versions of models.
+Copyright (C) 2014 Andreas Genz
+
+PSSIF Consistency is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PSSIF Consistency is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PSSIF Consistency.  If not, see <http://www.gnu.org/licenses/>.
+
+Feel free to contact me via eMail: genz@in.tum.de
+*/
+
+/**
  * Copyright 2004 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- pssif/pssif.consistency/src/main/java/org/pssif/textNormalization/Tokenizer.java
+++ pssif/pssif.consistency/src/main/java/org/pssif/textNormalization/Tokenizer.java
@@ -7,6 +7,26 @@ import java.util.List;
 import org.pssif.consistencyDataStructures.Token;
 
 /**
+This file is part of PSSIF Consistency. It is responsible for keeping consistency between different requirements models or versions of models.
+Copyright (C) 2014 Andreas Genz
+
+PSSIF Consistency is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PSSIF Consistency is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PSSIF Consistency.  If not, see <http://www.gnu.org/licenses/>.
+
+Feel free to contact me via eMail: genz@in.tum.de
+*/
+
+/**
  * This class implements a normalization step used in the normalizer class. This
  * class supplies a method "findTokens" that gets a string and returns the string as a list
  * of tokens.
